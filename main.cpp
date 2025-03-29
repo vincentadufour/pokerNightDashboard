a51e50e96049be6fb00b07ed5631e8ca880c4933
Vincent Dufour <avdufour@gmail.com>
2025-03-29 17:37:51 -0600

    Added buyChips and buyCash functions

--- main.cpp
+++ main.cpp
@@ -4,106 +4,190 @@
 #include <iostream>
 
 
+int getUserInt();
 
 class Player
 {
     public:
         std::string name {};
-        float cash {0};
-        std::map<std::string, int> chips {};
+        int cash {};
+        std::map<std::string, int> chips
+        {
+            {"red", 0},
+            {"white", 0},
+            {"green", 0},
+            {"black", 0}
+        };
+        std::string history {};
+
+        void recordHistory(std::string record)
+        {
+            history = history + record;
+        }
+
+        void printHistory()
+        {
+            std::cout << name << "'s History:\n" << history;
+        }
+
+        // chip map
+        std::map<std::string, int> chipMap
+        {
+            {"red", 1},
+            {"white", 5},
+            {"green", 20},
+            {"black", 50}
+        };
+
+
+        std::map<std::string, int> buyChips()
+        {
+            if ((cash % 100) != 0)
+            {
+                std::cout << "Please only buy chips in $100 denominations." << '\n';
+                return chips;
+            }
+
+            // how many purchases of $100 worth of chips to buy?
+            int purchases { cash / 100 };
+
+            // buying the chips
+            chips["red"] += 25 * purchases;
+            chips["white"] += 6 * purchases;
+            chips["green"] += 2 * purchases;
+
+            std::string message {name + " bought $" + std::to_string(cash) + " worth of chips.\n"};
+            recordHistory(message);
+
+            cash = 0;
+
+            return chips;
+        }
+
+
+        int buyCash()
+        {
+            cash = getUserInt();
+
+            std::string message {name + " invested $" + std::to_string(cash) + ".\n"};
+            recordHistory(message);
+
+            return cash;
+        }
+
+
+        void printChips()
+        {
+            std::cout << name << "'s chips:\n";
+            for (auto chip: chips)
+            {
+                std::cout << chip.first << ": " << chip.second << '\n';
+            }
+            std::cout << '\n';
+        }
+
+        void printCash()
+        {
+            std::cout << name << "'s cash:\n$" << cash << "\n\n";
+            
+        }
 };
 
 
-void printConsole()
+int getUserInt()
 {
-    std::string bar         {"#############################################################"};
-    std::string hollowBar   {"#                                                           #"};
-    
-    // std::cout << "\033[2J";
-    // std::cout << "\033[H";
-    std::cout << bar << '\n';
+    std::cout << "Please enter a value: " << '\n';
+    int input {};
+    std::cin >> input;
 
-    // top spacing
-    std::cout << hollowBar << '\n';
-
-    // title
-    std::cout <<        "#                   POKER NIGHT DASHBOARD                   #" << '\n';
-
-
-
-    // bottom spacing
-    for (int i {0}; i < 10; i++)
-    {
-        std::cout <<    "#                                                           #" << '\n';
-    }
-    
-    std::cout << bar << "\n\n";
+    return input;
 }
 
-std::list<std::string> playerCreation (std::list<std::string> playerList, int players)
-{    
-    std::cout << "\033[H";
 
-    for (int i {players}; i > 0; i--)
-    {
-        Player player[i];
+// void printConsole()
+// {
+//     std::string bar         {"#############################################################"};
+//     std::string hollowBar   {"#                                                           #"};
+    
+//     // std::cout << "\033[2J";
+//     // std::cout << "\033[H";
+//     std::cout << bar << '\n';
+
+//     // top spacing
+//     std::cout << hollowBar << '\n';
+
+//     // title
+//     std::cout <<        "#                   POKER NIGHT DASHBOARD                   #" << '\n';
+
+
+
+//     // bottom spacing
+//     for (int i {0}; i < 10; i++)
+//     {
+//         std::cout <<    "#                                                           #" << '\n';
+//     }
+    
+//     std::cout << bar << "\n\n";
+// }
+
+// std::list<std::string> playerCreation (std::list<std::string> playerList, int players)
+// {    
+//     std::cout << "\033[H";
+
+//     for (int i {players}; i > 0; i--)
+//     {
+//         Player player[i];
         
-        // name
-        std::string input {};
-        std::cout << "Player name: ";
-        std::cin >> input;
-        player[i].name = input;
+//         // name
+//         std::string input {};
+//         std::cout << "Player name: ";
+//         std::cin >> input;
+//         player[i].name = input;
 
-        // starting cash
+//         // starting cash
 
-        int amount {};
-        std::cout << "How much cash?: ";
-        std::cin >> amount;
-        player[i].cash = amount;
+//         int amount {};
+//         std::cout << "How much cash?: ";
+//         std::cin >> amount;
+//         player[i].cash = amount;
 
-    }
+//     }
 
-    std::cout << player1.name;
+//     std::cout << player1.name;
 
-    return playerList;
-}
+//     return playerList;
+// }
 
 
 
 
 int main()
 {
-    // chip map
-    std::map<std::string, int> chipMap
-    {
-        {"black", 100},
-        {"red", 50},
-        {"blue", 25},
-        {"white", 10}
-    };
-
     
     // preps player creation subroutine
     bool playerCreationRoutine { true };
 
-    // clears terminal
-    // std::cout << "\033[2J";
+    Player Lian;
+    Lian.name = "Lian";
+    Lian.buyCash();
+    Lian.printCash();
+    Lian.buyChips();
+    Lian.printChips();
+    Lian.printCash();
 
-    // player creation start
-    int input {};
-    std::cout << "How many players would you like to add?: ";
-    std::cin >> input;
-
-
-    std::list<std::string> playerList {};
-    playerList = playerCreation(playerList, input);
-
-    
-
-
-
-
+    Lian.printHistory();
 
 
     return 0;
 }
+
+
+/*
+Steps:
+Player class with class functions:
+- buy chips
+- sell chips
+- show stats
+- show all player's stats
+
+*/
